22b6982c2a8f3bd88862cbbd11ac497eeca138b7
leetaesk <itasuk@naver.com>
2025-06-24 23:27:56 +0900

    eslint.config.js 수정

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,39 @@
-import js from '@eslint/js';
-import eslintConfigPrettier from 'eslint-config-prettier';
+import eslint from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'node_modules/*', '!src/**/*'] },
+
+  // JS / TS Recommended
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+
+  // React Recommended + React 17 JSX-Runtime
+  react.configs.flat.recommended,
+  react.configs.flat['jsx-runtime'],
+
+  // Hooks preset (v5.2.0)
+  reactHooks.configs['recommended-latest'],
+
+  // Vite 전용 Fast-Refresh preset
+  reactRefresh.configs.vite,
+
+  // 프로젝트 전용(추가) 규칙
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
       globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      parserOptions: { ecmaFeatures: { jsx: true } },
     },
   },
+
+  // Prettier 충돌 제거
   eslintConfigPrettier,
 );
