b949082194eba987b4225ccb0463ff04edc6b98c
leetaesk <itasuk@naver.com>
2025-11-27 14:40:13 +0900

    feat:SpeedInsights 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 // import { StrictMode } from 'react';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import { createRoot } from 'react-dom/client';
 
 import App from '@/App.tsx';
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
     {/* <StrictMode> */}
     <App />
+    <SpeedInsights />
     <ReactQueryDevtools initialIsOpen={false} />
     {/* </StrictMode> */}
   </QueryClientProvider>,
