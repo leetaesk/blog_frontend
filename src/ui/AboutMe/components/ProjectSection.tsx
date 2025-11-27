8aac91c3027b18a155c520931555f8138aa3e34e
leetaesk <itasuk@naver.com>
2025-11-27 20:50:19 +0900

    feat:AboutMePage ui - 반응형

--- src/ui/AboutMe/components/ProjectSection.tsx
+++ src/ui/AboutMe/components/ProjectSection.tsx
@@ -1,33 +1,39 @@
 import ArrowRightIcon from '@/assets/icons/ArrowRightIcon';
-import AlogoBlack from '@/assets/images/ALogoBlack.svg';
-import AlogoWhite from '@/assets/images/ALogoWhite.svg';
 import grapPt from '@/assets/images/GrabPTLogo.svg';
-import useThemeStore from '@/store/themeStore';
+import LogoImage from '@/components/LogoImage';
 
 const ProjectSection = () => {
-  const isDark = useThemeStore((s) => s.theme) === 'dark';
-  const BlogLogo = isDark ? AlogoWhite : AlogoBlack;
   return (
-    <div className="mx-auto w-full px-4">
+    <div className="mx-auto w-full">
       {/* 그랩피티 */}
-      <div className="bg-card mx-auto h-64 max-w-4xl overflow-hidden rounded-xl shadow-lg md:flex">
-        <div className="flex items-center justify-center p-4 md:w-1/3">
-          <img className="h-full object-cover" src={grapPt} alt="grabPT project screenshot" />
+      <div className="bg-card mx-auto flex h-40 max-w-4xl overflow-hidden rounded-xl shadow-lg sm:h-64">
+        {/* 로고 안 짤리게 min-w 하드코딩 */}
+        <div className="flex w-1/3 items-center justify-center py-4 sm:min-w-[150.76px]">
+          <a
+            rel="noopener"
+            href="http://www.grabpt.com"
+            target="_blank"
+            className="text-primary group h-full object-cover font-medium group-hover:underline"
+          >
+            <img className="h-full object-contain" src={grapPt} alt="grabPT project screenshot" />
+          </a>
         </div>
-        <div className="flex flex-col gap-2 p-8 md:w-2/3">
-          <h3 className="text-card-foreground mt-1 text-4xl font-bold">
+        <div className="flex w-2/3 flex-col gap-1 p-2 sm:gap-2 sm:p-7 lg:p-8">
+          <h3 className="text-card-foreground mt-1 text-base leading-1 font-bold sm:text-3xl lg:text-4xl">
             grabPT <span className="font-normal">|</span>{' '}
-            <span className="text-xl">원하는 가격·방식으로 나만의 PT 매칭</span>
+            <span className="text-sm lg:text-xl">원하는 가격·방식으로 나만의 PT 매칭</span>
           </h3>
-          <div className="text-primary text-sm font-semibold tracking-wide">FRONT-END LEAD</div>
-          <p className="text-muted-foreground mt-2 text-sm leading-4">
+          <div className="text-primary text-xs font-semibold tracking-wide sm:text-sm">
+            FRONT-END LEAD
+          </div>
+          <p className="text-muted-foreground mt-3 text-sm leading-4 sm:mt-2">
             <p className="text-[#003efb]">
               Grab the weight. <br />
               Grab the challenge. <br />
               Grab the life you deserve <br />
             </p>
           </p>
-          <div className="group mt-4 flex cursor-pointer items-center text-[1rem]">
+          <div className="group mt-4 hidden cursor-pointer items-center text-[1rem] sm:flex">
             <a
               rel="noopener"
               href="http://www.grabpt.com"
@@ -42,22 +48,20 @@ const ProjectSection = () => {
       </div>
 
       {/* 내 블로그 */}
-      <div className="bg-card mx-auto mt-8 h-64 max-w-4xl overflow-hidden rounded-xl shadow-lg md:flex">
-        <div className="flex items-center justify-center p-4 md:w-1/3">
-          <img
-            className="h-full object-cover"
-            src={BlogLogo}
-            alt="Leeteesk's Archive Project Logo"
-          />
+      <div className="bg-card mx-auto mt-8 flex h-40 max-w-4xl overflow-hidden rounded-xl shadow-lg sm:h-64">
+        <div className="flex w-1/3 items-center justify-center py-4 sm:min-w-[150.76px]">
+          <LogoImage className="h-full object-cover" />
         </div>
-        <div className="flex flex-col gap-2 p-8 md:w-2/3">
-          <h3 className="text-card-foreground mt-1 text-4xl font-bold">Leetaesk's ARCHIVE</h3>
+        <div className="flex w-2/3 flex-col gap-2 p-2 sm:p-7 lg:p-8">
+          <h3 className="text-card-foreground mt-1 text-lg font-bold sm:text-3xl lg:text-4xl">
+            Leetaesk's ARCHIVE
+          </h3>
           <div className="text-primary text-sm font-semibold tracking-wide">Personal Project</div>
           <p className="text-muted-foreground mt-2 leading-4">
             React, Node.JS와 express를 활용하여 개인 블로그를 제작했습니다. <br />
             주로 기술적인 이야기를 다룹니다.
           </p>
-          <div className="group mt-4 flex cursor-pointer items-center text-[1rem]">
+          <div className="group mt-4 hidden cursor-pointer items-center text-[1rem] sm:flex">
             <a
               rel="noopener"
               href="http://www.grabpt.com"
