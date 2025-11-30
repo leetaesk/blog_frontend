37a11039fdb0ea66e447ea114f07703d3f77a76a
leetaesk <itasuk@naver.com>
2025-11-30 23:28:27 +0900

    feat:pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,43 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'node:path';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate', // 업데이트되면 자동으로 새 버전 적용
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: "Leetaesk's Archive",
+        short_name: 'Leetaesk',
+        description: '이태석의 개발 블로그',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable', // 아이콘이 꽉 차게 보이도록 함
+          },
+        ],
+        display: 'standalone', // ⭐️ 이게 있어야 주소창 없이 앱처럼 켜짐
+      },
+    }),
+  ],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
