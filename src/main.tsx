e98d8f3ece8415c1eacd87d19a410a0d33c307c6
leetaesk <itasuk@naver.com>
2025-12-03 22:12:00 +0900

    feat:vercel-analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 // import { StrictMode } from 'react';
 import { QueryClientProvider } from '@tanstack/react-query';
+import { Analytics } from '@vercel/analytics/next';
 // import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { SpeedInsights } from '@vercel/speed-insights/react';
 import { createRoot } from 'react-dom/client';
@@ -13,6 +14,7 @@ createRoot(document.getElementById('root')!).render(
     {/* <StrictMode> */}
     <App />
     <SpeedInsights />
+    <Analytics />
     {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     {/* </StrictMode> */}
   </QueryClientProvider>,
