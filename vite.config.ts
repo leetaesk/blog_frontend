030ed507b45d06634a3c229beef2afbd82b4a0b9
leetaesk <itasuk@naver.com>
2026-04-01 23:06:17 +0900

    chore: vercel

--- vite.config.ts
+++ vite.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: 'pwa-192x192-v2.png',
             sizes: '192x192',
             type: 'image/png',
           },
